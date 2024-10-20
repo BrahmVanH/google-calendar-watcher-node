19858fc6f35dd3c1d47c17541f85cbcd54d9ec86
Brahm Van Houzen <brahmvanh@gmail.com>
2024-10-20 14:10:28 -0400

    rebuilt after clearing some old unremoved built code

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ const { nodeProfilingIntegration } = require('@sentry/profiling-node');
 
 if (process.env.NODE_ENV === 'production') {
 	Sentry.init({
-		dsn: process.env.SENTRY_DNS ?? '',
+		dsn: process.env.SENTRY_DSN ?? '',
 		integrations: [nodeProfilingIntegration()],
 		// Tracing
 		tracesSampleRate: 1.0, //  Capture 100% of the transactions
